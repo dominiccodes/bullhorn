c5249fba1bc7f92c7cffe10fed0604e9edce95f5
dominiccodes <email@domcodes.com>
2017-08-17 08:05:47 +0100

    created shout model via scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :shouts
   devise_for :users
   get 'pages/home'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816204404) do
+ActiveRecord::Schema.define(version: 20170817070405) do
+
+  create_table "shouts", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
